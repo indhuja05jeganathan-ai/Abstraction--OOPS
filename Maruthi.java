e84d6b197b2691923457cd055bcacd30ba3417d4
indhuja05jeganathan-ai <indhuja05jeganathan@gmail.com>
2026-03-11 19:52:12 +0530

    Implement Maruthi class extending Car

---
+++ Maruthi.java
@@ -0,0 +1,22 @@
+package Abstraction;
+
+public class Maruthi extends Car{
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void run() {
+		System.out.println("marunthi running slow or medium");
+		
+	}
+
+	@Override
+	public void shutdown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
